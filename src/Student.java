4c0bc34ed52a367353f43a7714de62d45f7abe2b
SvileN1914 <svilen126@abv.bg>
2026-03-05 19:28:24 +0200

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+
+    private String name;
+    private String facultyNumber;
+    private String program;
+    private int year;
+    private int group;
+
+    public Student(String name, String facultyNumber, String program, int year, int group) {
+        this.name = name;
+        this.facultyNumber = facultyNumber;
+        this.program = program;
+        this.year = year;
+        this.group = group;
+    }
+
+    public void printStudent() {
+        System.out.println("Name: " + name);
+        System.out.println("Faculty number: " + facultyNumber);
+        System.out.println("Program: " + program);
+        System.out.println("Year: " + year);
+        System.out.println("Group: " + group);
+    }
+}
