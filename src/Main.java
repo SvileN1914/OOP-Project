74c2112f89b41dddc6621342d3053845518dc20c
SvileN1914 <svilen126@abv.bg>
2026-03-05 19:26:27 +0200

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student = new Student(
+                "Ivan Ivanov",
+                "1001",
+                "Informatics",
+                1,
+                1
+        );
+
+        student.printStudent();
+    }
+}
